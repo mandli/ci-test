6f9e1636d202a2773b2a380e26b41464851df776
Kyle Mandli <kyle.mandli@gmail.com>
2024-05-31 13:33:27 -0400

    Add some talkback

--- test_file.py
+++ test_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+import os
+
 import clawpack.geoclaw.surge.storm
 
+name = os.getenv("USER")
+CLAW = os.getenv("CLAW")
 print(clawpack.geoclaw.surge.storm.__file__)
+print(f"howdy {name}")
+print(f"My claw is at: {CLAW}")
